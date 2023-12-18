f95c9cd5c376437f6d9cbc9f5938eb02a6ed5842
cactus1998 <s770880qq@gmail.com>
2023-12-18 20:06:52 +0800

    完成

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -6,86 +6,119 @@ import SelectedProject from './components/SelectedProject.jsx';
 
 
 function App() {
-  const [projectsState,setProjectsState]=  useState({
-    selectedProjectId:undefined,
-    projects:[]
-  });
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectId: undefined,
+        projects: [],
+        tasks: []
+    });
 
-  function handleSelectProject(id) {
-    setProjectsState((prevState) =>{
+    function handleAddTask(text) {
+        setProjectsState((prevState) => {
+            const taskId = Math.random()
+            const newTask = {
+                text: text,
+                projectId: prevState.selectedProjectId,
+                id: taskId,
+            };
+            return {
+                ...prevState,
+                tasks: [newTask, ...prevState.tasks]
+            }
+        });
+    }
+
+    function handleDeleteTask(id) {
+        setProjectsState((prevState) => {
+            return {
+                ...prevState,
+                tasks: prevState.tasks.filter((task) => task.id !== id)
+    }
+});
+    }
+
+function handleSelectProject(id) {
+    setProjectsState((prevState) => {
         return {
             ...prevState,
-            selectedProjectId:id
+            selectedProjectId: id
         }
     });
-  }
+}
 
-  function handleStartAddProject() {
-    setProjectsState((prevState) =>{
+function handleStartAddProject() {
+    setProjectsState((prevState) => {
         return {
             ...prevState,
-            selectedProjectId:null
+            selectedProjectId: null
         }
     });
-  }
+}
 
-  function handleCancelAddProject() {
-    setProjectsState((prevState) =>{
+function handleCancelAddProject() {
+    setProjectsState((prevState) => {
         return {
             ...prevState,
-            selectedProjectId:undefined
+            selectedProjectId: undefined
         }
     });
-  }
+}
 
-  function handleAddProject(projectData) {
-    setProjectsState((prevState) =>{
-        const projectId=Math.random()
+function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+        const projectId = Math.random()
         const newProject = {
             ...projectData,
-            id:projectId
+            id: projectId
         };
-        return{
-            ...prevState,
-            selectedProjectId:projectId,
-            projects:[...prevState.projects,newProject ]
-        }
-    });
-  }
-
-  function handleDeleteProject(id) {
-    setProjectsState((prevState) =>{
         return {
             ...prevState,
-            selectedProjectId:undefined,
-            projects:prevState.projects.filter(
+            selectedProjectId: projectId,
+            projects: [...prevState.projects, newProject]
+        }
+    });
+}
+
+function handleDeleteProject() {
+    setProjectsState((prevState) => {
+        return {
+            ...prevState,
+            selectedProjectId: undefined,
+            projects: prevState.projects.filter(
                 (project) => project.id !== prevState.selectedProjectId
             )
         }
     });
-  }
+}
 
-  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
+const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
 
-  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}></SelectedProject>;
+let content = <SelectedProject
+    project={selectedProject}
+    onDelete={handleDeleteProject}
+    onAddTask={handleAddTask}
+    onDeleteTask={handleDeleteTask}
+    tasks={projectsState.tasks}
+>
+</SelectedProject>;
 
-  if(projectsState.selectedProjectId===null){
-    content=<NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
-  }else if(projectsState.selectedProjectId===undefined){
-    content=<NoProjectSelected onStartAddProject={handleStartAddProject} />
-  }
+if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+} else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+}
 
-  return (
+return (
     <main className='h-screen my-8 flex gap-8'>
-    <ProjectsSidebar 
-        onStartAddProject={handleStartAddProject} 
-        projects={projectsState.projects} 
-        onSelectProject={handleSelectProject}
-    />
-    {content}
-    {/* <NewProject /> */}
+        <ProjectsSidebar
+            onStartAddProject={handleStartAddProject}
+            projects={projectsState.projects}
+            onSelectProject={handleSelectProject}
+            selectedProjectId={projectsState.selectedProjectId}
+        />
+        {content}
+        {/* <NewProject /> */}
     </main>
-  );
+);
 }
 
 export default App;
--- 01-starting-project/src/components/Tasks.jsx
+++ 01-starting-project/src/components/Tasks.jsx
@@ -1,11 +1,25 @@
 import NewTask from "./NewTask";
 
-export default function Tasks() {
-    return(
+export default function Tasks({ tasks, onAdd, onDelete }) {
+    return (
         <section>
             <h2 className=" text-2xl font-bold text-stone-700 mb-4">NEW TASK</h2>
-            <NewTask></NewTask>
-            <p className=" text-stone-800 my-4">This project does not have any task yet.</p>
+            <NewTask onAdd={onAdd}></NewTask>
+            {tasks.length === 0 && (
+                <p className=" text-stone-800 my-4">This project does not have any task yet.</p>
+            )}
+            {tasks.length > 0 &&
+                <ul className="p-4 mt-8 rounded-md bg-stone-100">
+                    {tasks.map((task) =>
+                        <li key={task.id} className="flex justify-between my-4">
+                            <samp>{task.text}</samp>
+                            <button className=" text-stone-700 hover:text-red-500"
+                            onClick={()=>onDelete(task.id)}
+                            >Clear</button>
+                        </li>
+                    )}
+                </ul>
+            }
             <ul></ul>
         </section>
     )
